6a51358acadbf56c7d6dd7883afa8f121ed3f86c
mohamedrafat <mtag66870@gmail.com>
2026-01-26 10:31:54 +0200

    Finish get all products at home screen

---
+++ lib/features/home/presentation/managers/get_all_products_Cubit/get_all_products_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'get_all_products_state.dart';
+
+class GetAllProductsCubit extends Cubit<GetAllProductsState> {
+  GetAllProductsCubit() : super(GetAllProductsInitial());
+}
---
+++ lib/features/home/presentation/managers/get_all_products_Cubit/get_all_products_state.dart
@@ -0,0 +1,6 @@
+part of 'get_all_products_cubit.dart';
+
+@immutable
+sealed class GetAllProductsState {}
+
+final class GetAllProductsInitial extends GetAllProductsState {}
